2e435fdc9a85f09ea8c6b0ac4d210de62f83179b
debbiejansen <debbiejansen94@hotmail.com>
2025-11-30 14:14:16 +0100

    form fields toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [nameValue, setNameValue] = useState('');
+    const [ageValue, setAgeValue] = useState('');
+    const [messageValue, setMessageValue] = useState('');
+    const [termsAndConditionsValue, setTermsAndConditionsValue] = useState(false);
     return (
-        <div>
-            Maak hier jouw formulier!
-        </div>
+        <>
+            <form>
+                <fieldset>
+                    <legend>Gegevens</legend>
+                    <label htmlFor="name-field">
+                        Naam:
+                        <input
+                            type="text"
+                            value={nameValue}
+                            onChange={(e) => setNameValue(e.target.value)}
+                            id="name-field"
+                            name="name"
+                        />
+                    </label>
+                    <br/><br/>
+                    <label htmlFor="age-field">
+                        Leeftijd:
+                        <input
+                            type="text"
+                            value={ageValue}
+                            onChange={(e) => setAgeValue(e.target.value)}
+                            id="age-field"
+                            name="age"
+                            placeholder="0"
+                        />
+                    </label>
+                </fieldset>
+            </form>
+            <form>
+                <fieldset>
+                    <legend>Jouw review</legend>
+                    <label htmlFor="message-field">
+                        Opmerkingen:
+                        <input
+                            type="text"
+                            value={messageValue}
+                            onChange={(e) => setMessageValue(e.target.value)}
+                            id="message-field"
+                            name="message"
+                            placeholder="Wat vond je van het recept?"
+                        />
+                    </label>
+                    <br /><br />
+                    <label htmlFor="form-terms-and-conditions">
+                        <input
+                            type="checkbox"
+                            id="form-terms-and-conditions"
+                            name="terms-and-conditions"
+                            checked={termsAndConditionsValue}
+                            onChange={() => setTermsAndConditionsValue(!termsAndConditionsValue)}
+                        />
+                        Ik ga akkoord met de algemene voorwaarden
+                    </label>
+                </fieldset>
+            </form>
+        </>
     );
 }
 
